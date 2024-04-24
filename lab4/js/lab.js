3069231a0a41a0043f0931e343840f0e5671f95b
ggzell707 <166079135+ggzell707@users.noreply.github.com>
2024-04-24 01:10:29 -0700

    Lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,11 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Snake Game 
+// Author: Giselle Torres
+// Date: April 2024
+
+
+//Print board
+  //Spawn snake and food at specific coordinates on board	
+  //Create walls
+  //Set snake directed towards right
+  //Display score
+
+//Commence game upon user input 
+  //Accept user input to be able to move snake around in all 4 directions 
+  //Include collision with the player and the borders
+  //Evaluate player growth upon consuming food 
+    //Display score increasing by one unit upon consumption
+  //Randomize food spawn after consumption
+
+//Lose Game
+  //Display final score and option to restart the game after death 
 
 // Constants
-
 // Functions
-
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
