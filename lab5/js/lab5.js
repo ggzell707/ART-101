d60eb1db565570d557969806e11f1c3e267af65f
ggzell707 <166079135+ggzell707@users.noreply.github.com>
2024-04-26 00:40:15 -0700

    Lab5

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+// index.js - purpose and description here
+// Author: Giselle Torres
+// Date:4/25/2024
+
+//Define Variables
+let make = "Ford";
+let model = "Focus";
+let color = "red";
+let year = 2014;
+let ownIt = true;
+let CurrentYear= 2024;
+
+// calculate
+age= 2024-year;
+
+// Output variables using document.writeln()
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
