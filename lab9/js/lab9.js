3d98dc2d2888915fb448e86f8e6419ab6e95c696
ggzell707 <166079135+ggzell707@users.noreply.github.com>
2024-05-13 22:36:13 -0700

    Lab 9

---
+++ lab9/js/lab9.js
@@ -0,0 +1,26 @@
+// index.js - Library and Jquery
+// Author: Giselle Torres
+// Date: May 13, 2024
+
+// add button to challenge section
+$("#button-challenge").click(function(){
+//change style of challenge sections
+    $("#challenge").toggleClass("special");
+});
+
+
+// add button to problems section
+$("#button-problems").click(function(){
+//change style of problems sections
+    $("#problems").toggleClass("special");
+});
+
+
+// add button to results section
+$("#button-results").click(function(){
+//change style of results sections
+    $("#results").toggleClass("special");
+});
+ 
+
+
