e59917015f235eb9c930c3c34a0a0ee5eba3d697
ggzell707 <166079135+ggzell707@users.noreply.github.com>
2024-05-15 14:09:30 -0700

    jhi

---
+++ experiments/js/experiments.js
@@ -0,0 +1,8 @@
+    console.log 
+       function getNameAndDisplay() {
+        let name = prompt ("Name please:")
+        $('#title').html('Hello, ' + name);
+        alert('Hello')
+    }
+    
+  //  $("my-button").click()
